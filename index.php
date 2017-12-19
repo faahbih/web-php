a9c5bda905b2906d8f1347e5d55a11d0cd65a57c
Fabiana Oliveira <fabiana.rr.oliveira@gmail.com>
2017-12-19 16:09:48 -0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,403 @@
+
+
+<table width="780" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td colspan="3" valign="top"><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+			<html>
+			<head>
+				<title>Cursos de Curta Duração</title>
+				<script language="JavaScript">
+					<!--
+function MM_preloadImages() { //v3.0
+	var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+		var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+		if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+	}
+function MM_swapImgRestore() { //v3.0
+	var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_findObj(n, d) { //v3.0
+	var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+		d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+		if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+		for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document); return x;
+	}
+function MM_swapImage() { //v3.0
+	var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+	if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+function MM_displayStatusMsg(msgStr) { //v1.0
+	status=msgStr;
+	document.MM_returnValue = true;
+}
+//-->
+</script>
+<script>
+	function instru(qtURL)
+	{
+		windowqt = window.open(qtURL, "","toolbar=no,location=no,status=no,menubar=no,width=500,height=728, resizable=no,scrollbars=no");
+		windowqt.focus();
+	}
+</script>
+<script>
+	function instru1(qtURL)
+	{
+		windowqt = window.open(qtURL, "","toolbar=no,location=no,status=no,menubar=no,width=400,height=600, resizable=no,scrollbars=no");
+		windowqt.focus();
+	}
+</script>
+<script language="JavaScript">
+	function checar_formulario() 
+	{
+		if (document.form1.nome.value == "")
+		{
+			alert("É preciso informar o Nome!");
+			document.form1.nome.focus();
+			return false;
+		}
+		if (document.form1.empresa.value == "")
+		{
+			alert("É preciso informar a Empresa!");
+			document.form1.empresa.focus();
+			return false;
+		}
+		if (document.form1.endereco.value == "")
+		{
+			alert("É preciso informar o Endereço!");
+			document.form1.endereco.focus();
+			return false;
+		}
+		if (document.form1.cidade.value == "")
+		{
+			alert("É preciso informar a Cidade!");
+			document.form1.cidade.focus();
+			return false;
+		}
+		if (document.form1.estado.value == "")
+		{
+			alert("É preciso informar o Estado!");
+			document.form1.estado.focus();
+			return false;
+		}
+		if (document.form1.cep1.value == "")
+		{
+			alert("É preciso informar o CEP!");
+			document.form1.cep1.focus();
+			return false;
+		}
+		if (document.form1.pais.value == "")
+		{
+			alert("É preciso informar o País!");
+			document.form1.pais.focus();
+			return false;
+		}
+		if (document.form1.email.value == "")
+		{
+			alert("É preciso informar o Email!");
+			document.form1.email.focus();
+			return false;
+		}
+		if (document.form1.ddd.value == "")
+		{
+			alert("É preciso informar o DDD!");
+			document.form1.ddd.focus();
+			return false;
+		}
+		if (document.form1.fone.value == "")
+		{
+			alert("É preciso informar o Telefone!");
+			document.form1.fone.focus();
+			return false;
+		}
+		if (document.form1.datac.value == "")
+		{
+			alert("É preciso escolher uma Data!");
+			document.form1.datac.focus();
+			return false;
+		}
+
+	}
+	function checar_curso() 
+	{
+		if (document.form2.curso.value == "")
+		{
+			alert("É preciso selecionar o curso!");
+			document.form2.curso.focus();
+			return false;
+		}
+	}
+	function checar_login() 
+	{
+		if (document.form3.usuario.value == "")
+		{
+			alert("É preciso informar o usuário!");
+			document.form3.usuario.focus();
+			return false;
+		}
+		if (document.form3.senha.value == "")
+		{
+			alert("É preciso informar a senha!");
+			document.form3.senha.focus();
+			return false;
+		}
+	}
+</script>
+</head>
+<STYLE type=text/css>
+p { font-size: 8pt; font-family: Arial; text-align: justify}
+.pmin { font-size: 8pt; font-family: Arial; text-align: justify }
+li { font-size: 8pt; font-family: Arial; text-align: justify}
+td { font-size: 8pt; font-family: Arial; text-align: center}
+LINK {FONT-FAMILY: Arial, Helvetica, sans-serif; font-color: color=#000000 ;}
+.tabmenor {font-size: 8pt; font-family: Arial; font-color: color=#000000 ;text-align: center}
+h3 {font-size: 8pt; font-family: Arial; text-align: center; TEXT-DECORATION: underline}
+A:link { font-color: color=#000000 ;}
+A:hover { font-color: color=#000000 ;}
+A:visited { font-color: color=#000000 ;}
+</style>
+<body bgcolor="#cfffcf" leftmargin="0" topmargin="0" bottommargin="0" onLoad="MM_preloadImages('img/home_on.gif', 'img/proposta_on.gif','img/hoteis_on.gif','img/docentes_on.gif','img/bibliografia_on.gif')">
+	<table width="100%" border="0" cellspacing="0" cellpadding="0">
+		<tr>
+			<!-- <td colspan="2"><a href="http://www.selperbrasil.org.br/" target="_blank"><img src="#" alt="" border="0"></a><img src="img/3-CURSO-CURTA-DURAÇÃO.jpg" alt="Cursos de Curta Duração" border="0"><a href="http://www.inpe.br/" target="_blank"><img src="img/superior_inpe.jpg" alt="" border="0"></a></td> -->
+			<img src="img/3-CURSO-CURTA-DURAÇÃO.jpg" alt="">
+		</tr>
+		<tr>
+			<td colspan="2"><a href="index.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('home','','img/home_on.gif',1);return document.MM_returnValue"><img src="img/home.gif" name="home" border="0"></a><a href="proposta.php" onMouseOver="MM_swapImage('proposta','','img/proposta_on.gif',1);return document.MM_returnValue" onMouseOut="MM_swapImgRestore()"><img src="img/proposta.gif" name="proposta" border="0"></a><a href="docentes.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('docentes','','img/docentes_on.gif',1);return document.MM_returnValue"><img src="img/docentes.gif" name="docentes" border="0"></a><a href="bibliografia.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('bibliografia','','img/bibliografia_on.gif',1);return document.MM_returnValue"><img src="img/bibliografia.gif" name="bibliografia" border="0"></a><a href="contato.php" onMouseOver="MM_swapImage('contato','','img/contato_on.gif',1);return document.MM_returnValue" onMouseOut="MM_swapImgRestore()"><img src="img/contato.gif" name="contato" border="0"></a></td>
+		</tr>
+		<tr>
+			<td height="1" colspan="2" bgcolor="#000000"></td>
+		</tr>
+	</table>
+
+</td>
+</tr>
+<tr>
+	<td width="175" valign="top" bgcolor="#61b78e">
+		<table width="90%" cellspacing="0" cellpadding="0" align="center">
+			<tr>
+				<td height="10"></td>
+			</tr>
+			<tr>
+				<td height="20" bgcolor="#006633"><div align="center"><font color="#ffffff"><strong>CURSOS SJC</strong></div></td>
+				</tr>	
+				<tr>
+					<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="cursos_sjc.php" style="text-decoration: none;"><font color="#ffffff">Cursos Disponíveis</font></a></div></td>
+				</tr>
+				<tr>
+					<td height="1" bgcolor="#006633"></td>
+				</tr>
+				<tr>
+					<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="cronograma_sjc.php" style="text-decoration: none;"><font color="#ffffff">Cronograma por curso</font></a></div></td>
+				</tr>
+				<tr>
+					<td height="1" bgcolor="#006633"></td>
+				</tr>
+				<tr>
+					<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="cronograma_data_sjc.php" style="text-decoration: none;"><font color="#ffffff">Cronograma por data</font></a></div></td>
+				</tr>
+				<tr>
+					<td height="1" bgcolor="#006633"></td>
+				</tr>
+				<tr>
+					<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="inscricao_sjc.php" style="text-decoration: none;"><font color="#ffffff">Inscrição</font></a></div></td>
+				</tr>
+				<tr>
+					<td height="1" bgcolor="#006633"></td>
+				</tr>
+				<tr>
+					<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="local_sjc.php" style="text-decoration: none;"><font color="#ffffff">Local de Realização</font></a></div></td>
+				</tr>
+				<tr>
+					<td height="1" bgcolor="#006633"></td>
+				</tr>
+				<tr>
+					<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="instalacoes_sjc.php" style="text-decoration: none;"><font color="#ffffff">Instalações</font></a></div></td>
+				</tr>
+				<tr>
+					<td height="1" bgcolor="#006633"></td>
+				</tr>
+				<tr>
+					<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="hoteis.php" style="text-decoration: none;"><font color="#ffffff">Sugestão de Hotéis</font></a></div></td>
+				</tr>
+
+
+<!--
+	<tr>
+		<td height="20" bgcolor="#006633"><div align="center"><font color="#ffffff"><strong>CURSOS BELÉM</strong></div></td>
+	</tr> 
+ 	<tr>
+		<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="cursos_belem.php" style="text-decoration: none;"><font color="#ffffff">Cursos Disponíveis</font></a></div></td>
+	</tr>
+	<tr>
+		<td height="1" bgcolor="#006633"></td>
+	</tr>
+	<tr>
+		<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="cronograma_belem.php" style="text-decoration: none;"><font color="#ffffff">Cronograma por curso</font></a></div></td>
+	</tr>
+	<tr>
+		<td height="1" bgcolor="#006633"></td>
+	</tr>
+    <tr>
+		<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="cronograma_data_belem.php" style="text-decoration: none;"><font color="#ffffff">Cronograma por data</font></a></div></td>
+	</tr>
+	<tr>
+		<td height="1" bgcolor="#006633"></td>
+	</tr>
+	<tr>
+		<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="inscricao_belem.php" style="text-decoration: none;"><font color="#ffffff">Inscrição</font></a></div></td>
+	</tr>
+	<tr>
+		<td height="1" bgcolor="#006633"></td>
+	</tr>
+	<tr>
+		<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="local_belem.php" style="text-decoration: none;"><font color="#ffffff">Local de Realização</font></a></div></td>
+	</tr>
+	<tr>
+		<td height="1" bgcolor="#006633"></td>
+	</tr>
+	<tr>
+		<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="hoteis_belem.php" style="text-decoration: none;"><font color="#ffffff">Sugestão de Hotéis</font></a></div></td>
+	</tr>
+    
+-->
+<tr>
+	<td height="20" bgcolor="#006633"><div align="center"><img src="img/bt_geral.gif" alt="" border="0"></div></td>
+</tr>
+<tr>
+	<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="normas.php" style="text-decoration: none;"><font color="#ffffff">Normas / Procedimentos</font></a></div></td>
+</tr>
+<tr>
+	<td height="1" bgcolor="#006633"></td>
+</tr>
+<tr>
+	<td height="20"><div align="left"><img src="img/dot.gif" alt="" border="0">&nbsp;<a href="curso_fora.php" style="text-decoration: none;"><font color="#ffffff">Cursos na sua Instituição</font></a></div></td>
+</tr>
+
+
+</table>
+
+</td>
+<td width="445" height="400" valign="top" bgcolor="#FFFFFF">
+
+		<!--- Início Meio --->
+		
+			<table width="95%" align="center">
+				<tr>
+					<td>
+					<div align="justify">
+					<br>
+					<font size="2">
+
+<img src="img/barra_for.gif" alt="" border="0"><br><br>
+              O programa de Formação Avançada em Geotecnologias é oferecido pela 
+              <a href="http://www.selperbrasil.org.br/" target="_blank" style="text-decoration: none;"><font color="#006633">SELPER 
+              Brasil</font></a> e tem por objetivo oferecer um conjunto de cursos 
+              de curta duração, destinados a apoiar o uso das geotecnologias no 
+              Brasil.<br>
+               <br>
+<strong>Nossa Proposta</strong><br><br>
+              Uma das atividades estatut&aacute;rias da SELPER &eacute; promover 
+              e estimular o desenvolvimento, estudo e pesquisa no campo de sensoriamento 
+              remoto, geotecnologia, geoprocessamento e &aacute;reas afins, no 
+              sentido amplo, abrangendo as fases de aquisi&ccedil;&atilde;o, tratamento, 
+              an&aacute;lise e interpreta&ccedil;&atilde;o dos dados, bem como 
+              a respectiva divulga&ccedil;&atilde;o das informa&ccedil;&otilde;es 
+              obtidas. <br>
+			  <br>
+              Em raz&atilde;o da singularidade de seu campo de atua&ccedil;&atilde;o, 
+                a SELPER atua como disseminadora dos conhecimentos das t&eacute;cnicas 
+                de sensoriamento remoto, geotecnologias e geoprocessamento. Para 
+                tanto, promove eventos, como semin&aacute;rios, workshops, simp&oacute;sios, 
+                cursos, palestras e outras atividades afins, no intuito de incentivar 
+                a produ&ccedil;&atilde;o de trabalhos cient&iacute;ficos afetos 
+                &agrave; sua &aacute;rea de atua&ccedil;&atilde;o, bem como para 
+                reunir os especialistas do Brasil para interagirem entre si e 
+                tamb&eacute;m com estudantes e outros profissionais da &aacute;rea.<br>
+				<br>
+              As t&eacute;cnicas de sensoriamento remoto, geotecnologias e 
+                geoprocessamento v&ecirc;m sendo amplamente utilizadas nas mais 
+                diversas atividades econ&ocirc;micas e governamentais, fornecendo 
+                subs&iacute;dios adequados de planejamento e gest&atilde;o de 
+                projetos de desenvolvimento sustent&aacute;vel, e t&ecirc;m se 
+                tornado ferramentas essenciais para avalia&ccedil;&atilde;o, planejamento 
+                e monitoramento das interven&ccedil;&otilde;es humanas no meio 
+                f&iacute;sico que conduzam &agrave; sustentabilidade. As referidas 
+                t&eacute;cnicas s&atilde;o utilizadas, por exemplo, no planejamento 
+                urbano, monitoramento ambiental e de recursos naturais, monitoramento 
+                agr&iacute;cola, meteorologia, entre outras diversas aplica&ccedil;&otilde;es.<br>
+				<br>
+              Atualmente, com a preocupa&ccedil;&atilde;o que existe em rela&ccedil;&atilde;o 
+                ao desenvolvimento sustent&aacute;vel, tem sido crescente a demanda 
+                por cursos e eventos para &oacute;rg&atilde;os p&uacute;blicos 
+                e empresas privadas, dentre outros, que desempenham atividades 
+                voltadas &agrave; preserva&ccedil;&atilde;o do meio ambiente.<br>
+				<br>
+              &Eacute; nesse contexto que a SELPER Brasil oferece um programa 
+                de treinamento cujo objetivo &eacute; oferecer um conjunto de 
+                cursos de curta dura&ccedil;&atilde;o, destinados a apoiar o uso 
+                das t&eacute;cnicas de Sensoriamento Remoto, Geotecnologias e 
+                Geoprocessamento no Brasil.<br>
+				<br>
+              Merece destaque o fato de aproximadamente 4.700 profissionais 
+                terem participado dos cursos nos quinze anos de exist&ecirc;ncia 
+                do nosso programa de treinamento.<br>
+             <br>
+              Os cursos de curta duração estão abertos a todos os interessados 
+              e são ministrados em S&atilde;o Jos&eacute; dos Campos (SP) ou na 
+              sua <a href="curso_fora.php"><font color="#006633">instituição</font></a>. 
+              <br>
+              <br><br>
+
+</div>
+</font>
+					</div>
+					</td>
+				</tr>
+			</table>
+			
+		<!--- Fim Meio --->
+		
+		</td>
+		<td width="160" valign="top" bgcolor="#61b78e">
+			<table width="90%" cellspacing="0" cellpadding="0" align="center">
+	<tr>
+		<td height="10"></td>
+	</tr>
+	<tr>
+		<td height="20" bgcolor="#006633"><div align="center"><img src="img/bt_links.gif" alt="" border="0"></div></td>
+	</tr>
+	<tr>
+		<td><br></td>
+	</tr>
+
+<tr>
+		<td align="center"><br><a href="http://www.selperbrasil.org.br/cursos/ead/sr_ac/" target="_blank"><img src="logo_SRAC_pq.jpg" alt="" border="0"></a><br><br></td>
+	</tr>
+
+<!--	
+	<tr>
+		<td align="center"><br><a href="http://www.dpi.inpe.br/" target="_blank"><img src="img/logo_dpi.jpg" alt="" border="0"></a><br><br></td>
+	</tr>
+	<tr>
+		<td align="center"><br><a href="http://www.obt.inpe.br/" target="_blank"><img src="img/logo_obt.jpg" alt="" border="0"></a><br><br></td>
+	</tr>
+-->	
+
+
+</table>
+</td>
+</tr>
+<tr>
+	<td colspan="3" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+		<tr>
+			<td bgcolor="#61b78e"><a href="area_restrita.php"><img src="img/cadeado.gif" alt="" border="0"></a></td>
+			<td height="20" bgcolor="#61b78e"><div align="center"><font size="1" color="#FFFFFF">Atualizado em: 08/2015</font></div></td>
+		</tr>
+	</table>
+</td>
+</tr>
+</table>
+
+</body>
+</html>
